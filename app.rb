a37a20c2f09e58efd4f369bfae984bdd6d825ba4
Raimo Tuisku <raimo.tuisku@gmail.com>
2013-10-24 16:11:00 -0700

    Add JIRA call.

--- app.rb
+++ app.rb
@@ -20,7 +20,12 @@ class MyApp < Sinatra::Base
   post '/enabled' do
     request.body.rewind
     h = JSON.parse request.body.read
+    puts 'Getting these back from JIRA'
     ap h
+    ap 'Initializing new OAuth::Consumer using oauth gem'
+    consumer = OAuth::Consumer.new(h['clientKey'], h['publicKey'], private_key_file: 'private-key.pem', signature_method: 'RSA-SHA1', site: 'http://localhost:2990')
+    puts 'Making "find anything" call.'
+    ap consumer.request(:get, '/jira/rest/api/2/search')
   end
   run! if app_file == $0
 end
